4fd75e3bd69296f8fba471d8008075cea99f8e23
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-11 13:01:27 -0700

    added display scores and storage functionx

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,12 @@
 // access html through id
-var startButtonEL = document.querySelector('#startQuiz')
-var titleEL = document.querySelector("#title");
-var pageContentEL = document.querySelector("#page-content");
+var highScoresEL = document.querySelector("#viewHighScores")
+var timerEL = document.querySelector('#time-left');
+var startButtonEL = document.querySelector('#startQuiz');
 var quizContainerEL = document.querySelector("#quizContainer");
 var questionIdCounter = 0;
+var counter = 60;
 
-var highsores = [];
+var highscores = [];
 
 // quiz questions and answers in array
 var questions = [
@@ -51,20 +52,37 @@ var questions = [
     }
 ];
 
-// create function to replace html with quiz questions
+// create function to start timer once start quiz button is clicked
 function startQuiz() {
-    console.log("click");
+    // function to start timer
+    function timer() {
+        counter--;
+        timerEL.textContent = counter;
+        if (counter <= 0) {
+            console.log(counter);
+            timerEL.textContent = 0;
+            clearInterval(startTimer);
+            endQuiz();
+        }
+        else if (questionIdCounter >= questions.length) {
+            console.log(counter);
+            clearInterval(startTimer);
+            endQuiz();
+        }
+    }
 
-    titleEL.textContent = "";
+    var startTimer = setInterval(timer, 1000);
+
     displayQuestions();
 
 };
 
-// display questions
+// function to display the quiz questions
 function displayQuestions() {
     console.log(questions);
     quizContainerEL.textContent = "";
 
+    // creates a container to hold the questions
     var questionAnswerDiv = document.createElement('div');
     questionAnswerDiv.className = 'container';
 
@@ -96,10 +114,10 @@ function displayQuestions() {
     answer4.setAttribute("value", "answer4")
     questionAnswerDiv.appendChild(answer4);
 
+    // appends questionAnswer container to quiz contatiner Div
     quizContainerEL.appendChild(questionAnswerDiv);
 
     var questionAnswerDivEL = document.querySelector('.container');
-
     questionAnswerDivEL.addEventListener("click", checkAnswers);
 };
 
@@ -110,20 +128,21 @@ function checkAnswers(event) {
     answerSelected = answerEL.getAttribute("value");
     var lineEL = document.createElement("hr");
     var answerText = document.createElement("h3");
-    console.log(answerEL);
-    console.log(answerSelected);
 
-    // check for correct answer
-    if (answerSelected === questions[questionIdCounter].correctAnswer) {
-        questionIdCounter = questionIdCounter + 1;
-        answerText.textContent = "Correct!";
+    // check that only button was clicked
+    if (answerEL.matches(".btn")) {
+        // check for correct answer
+        if (answerSelected === questions[questionIdCounter].correctAnswer) {
+            questionIdCounter = questionIdCounter + 1;
+            answerText.textContent = "Correct!";
 
-    } else {
-        questionIdCounter = questionIdCounter + 1;
-        answerText.textContent = "Wrong!";
+        } else {
+            counter = counter - 5;
+            questionIdCounter = questionIdCounter + 1;
+            answerText.textContent = "Wrong!";
 
+        }
     }
-
 
     // check if there are more questions and display next questions
     if (questionIdCounter < questions.length) {
@@ -138,6 +157,145 @@ function checkAnswers(event) {
 
 };
 
+function endQuiz() {
+    var newHighscore = timerEL.textContent;
+    var highscoresContainer = document.createElement("div");
+    var completedQuiz = document.createElement("h2");
+    var scoreText = document.createElement("p");
+
+    // check if score = 0
+    if (newHighscore === "0") {
+        completedQuiz.textContent = "Game over! Timer ran out!";
+
+        var startOverButton = document.createElement("button");
+        startOverButton.textContent = "Try Again";
+        startOverButton.className = "btn start-over";
+
+        highscoresContainer.appendChild(startOverButton);
+        startOverButton.addEventListener("click", restartQuiz);
+    } else {
+        completedQuiz.textContent = "All Done!"
+        scoreText.textContent = "Your final score is " + newHighscore;
+
+        var highScoreForm = document.createElement("form");
+        var inputLabel = document.createElement("label");
+        inputLabel.for = "addHighscore";
+        inputLabel.textContent = "Enter your Initials";
+
+        var inputScore = document.createElement("input");
+        inputScore.type = "text";
+        inputScore.id = "addNewScore";
+        inputScore.name = "addHighScore";
+
+        var addScoreButton = document.createElement("button");
+        addScoreButton.textContent = "Add Score";
+        addScoreButton.className = "btn";
+        addScoreButton.id = "addScoreButton";
+
+        highScoreForm.appendChild(inputLabel);
+        highScoreForm.appendChild(inputScore);
+        highScoreForm.appendChild(addScoreButton);
+        highscoresContainer.appendChild(highScoreForm);
+    }
+
+    highscoresContainer.appendChild(completedQuiz);
+    highscoresContainer.appendChild(scoreText);
+    highscoresContainer.addEventListener("submit", addScoreFormHandler);
+    quizContainerEL.appendChild(highscoresContainer);
+}
 
 
-startButtonEL.addEventListener('click', startQuiz);
+function addScoreFormHandler(event) {
+    event.preventDefault();
+    console.log("clicked");
+    score = timerEL.textContent;
+
+    var scoreInitials = document.querySelector("input[name='addHighScore']").value;
+    console.log(scoreInitials);
+    var addHighScores = {
+        initials: scoreInitials,
+        highscore: score
+    }
+    highscores.push(addHighScores);
+    console.log(highscores);
+    saveScores();
+
+    // show saved scores
+    displayScores();
+};
+
+function saveScores() {
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+function loadScore() {
+    var loadHighScores = localStorage.getItem("highscores");
+    if (!loadHighScores) {
+        return false
+    }
+    loadHighScores = JSON.parse(loadHighScores);
+
+    for (i = 0; i < loadHighScores.length; i++) {
+        highscores.push(loadHighScores[i]);
+    }
+}
+
+function displayScores() {
+    quizContainerEL.textContent = "";
+
+    var displayScoresContainer = document.createElement("div");
+    var viewHighScore = document.createElement("h2");
+    viewHighScore.textContent = "High Scores";
+    displayScoresContainer.appendChild(viewHighScore);
+
+    var listScores = document.createElement("ol");
+
+    // loop to display scores
+    for (i = 0; i < highscores.length; i++) {
+        var eachScore = document.createElement("li");
+        eachScore.textContent = highscores[i].initials + " - " + highscores[i].highscore;
+        listScores.appendChild(eachScore);
+    }
+
+    var startOverButton = document.createElement("button");
+    startOverButton.textContent = "Try Again";
+    startOverButton.className = "btn start-over";
+
+    var clearButton = document.createElement("button");
+    clearButton.textContent = "Clear High Score";
+    clearButton.className = "btn";
+
+    displayScoresContainer.appendChild(listScores);
+    displayScoresContainer.appendChild(startOverButton);
+    displayScoresContainer.appendChild(clearButton);
+    quizContainerEL.appendChild(displayScoresContainer);
+
+    startOverButton.addEventListener("click", restartQuiz);
+    clearButton.addEventListener("click", clearScores);
+
+}
+
+
+// refreshes page to restart quiz
+function restartQuiz() {
+    location.reload();
+}
+// view high scores directly
+function viewScore(event) {
+    event.preventDefault();
+    displayScores();
+}
+// clears local storage
+function clearScores() {
+    localStorage.clear("quizScore");
+    alert("High Score cleared");
+    restartQuiz();
+}
+
+// load scores on page load
+loadScore();
+
+// event listener on start button quiz and to viewscores link
+startButtonEL.addEventListener("click", startQuiz);
+highScoresEL.addEventListener("click", viewScore);
+
